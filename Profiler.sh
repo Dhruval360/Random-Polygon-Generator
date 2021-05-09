cb5de012bbf81315449721c424e797be0f591f1e
Mihir Kestur <mihirkestur@outlook.com>
2021-05-09 22:21:55 +0530

    added metrics analysis for time and size

--- Profiler.sh
+++ Profiler.sh
@@ -1,5 +1,6 @@
 iterations=$2
-echo "Profiling $1 algorithm by running $2 iterations"
+#echo "Profiling $1 algorithm by running $2 iterations"
+echo "num_polygons,time,size,algorithm"
 for i in $(seq 1 $iterations); do 
     numPolygons=$((100*$i))
     ./bin/polygonGenerator -a $1 -n $numPolygons -p 1
--- metrics.py
+++ metrics.py
@@ -1,9 +1,30 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-df = pd.read_csv("test.csv")
-#for every algo
-#polygons vs time
-#polygons vs size
+fig, axes = plt.subplots(nrows=1, ncols=2)
+df = pd.read_csv("metrics.csv")
 
-df.plot(x='num', y='time')
+df_sp = df.loc[df["algorithm"] == " spacePartition"]
+df_pol = df.loc[df["algorithm"] == " polar"]
+#df_ch = df.loc[df["algorithm"] == " polar"]
+
+#space partition
+axes[0].plot(df_sp['num_polygons'], df_sp['size'], color="blue", label = "Space Partition")
+axes[1].plot(df_sp['num_polygons'], df_sp['time'], color="blue", label = "Space Partition")
+#polar
+axes[0].plot(df_pol['num_polygons'], df_pol['size'], color="red", label = "Polar")
+axes[1].plot(df_pol['num_polygons'], df_pol['time'], color="red", label = "Polar")
+#ch
+#axes[0].plot(df_pol['num_polygons'], df_pol['size'], color="red")
+#axes[1].plot(df_pol['num_polygons'], df_pol['time'], color="red")
+
+axes[0].legend(loc='upper left')
+axes[1].legend(loc='upper left')
+
+axes[0].set_xlabel('Number of polygons') 
+axes[0].set_ylabel('Size (in bytes)')
+
+axes[1].set_xlabel('Number of polygons') 
+axes[1].set_ylabel('Time (in seconds)')
+
+plt.suptitle("Metrics analysis")
 plt.show()
