666ffe629f87d1bcc024c7ada7e8db2d2d1d1438
Mihir Kestur <mihirkestur@outlook.com>
2021-05-10 19:36:55 +0530

    profiler runs both algorithms

--- Profiler.sh
+++ Profiler.sh
@@ -1,7 +1,8 @@
-iterations=$2
+iterations=$1
 #echo "Profiling $1 algorithm by running $2 iterations"
 echo "num_polygons,time,size,algorithm"
 for i in $(seq 1 $iterations); do 
     numPolygons=$((100*$i))
-    ./bin/polygonGenerator -a $1 -n $numPolygons -p 1
+    ./bin/polygonGenerator -a spacePartition -n $numPolygons -p 1
+    ./bin/polygonGenerator -a polar -n $numPolygons -p 1
 done
