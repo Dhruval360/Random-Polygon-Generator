9bb8cb4b449a7d9b772504d84dec73efd1254200
Dhruval360 <dhruvalpb@gmail.com>
2021-08-24 21:25:01 +0530

    Updated executable's location and handled the case when number of iterations isn't specified.

--- Profiler.sh
+++ Profiler.sh
@@ -1,15 +1,24 @@
-iterations=$1
+#!/bin/bash
+if [ "$#" -lt 1 ] 
+then 
+    echo "Number of iterations not specified. Using a default value of 5..."
+    echo 
+    iterations=5;    
+else 
+    iterations=$1
+fi
+
 echo "Profiling started..."
 echo "num_polygons,time,size,algorithm" > metrics.csv
 for i in $(seq 1 $iterations); do 
     numPolygons=$((100*$i))
     echo "Iteration $i (Generating $numPolygons polygons using each algorithm)"
     echo -e "\tExecuting Polar algorithm..."
-    ./bin/polygonGenerator -a polar -n $numPolygons -p 1 >> metrics.csv
+    ./build/bin/polygonGenerator -a polar -n $numPolygons -p 1 >> metrics.csv
     echo -e "\tExecuting Space Partition algorithm..."
-    ./bin/polygonGenerator -a spacePartition -n $numPolygons -p 1 >> metrics.csv
+    ./build/bin/polygonGenerator -a spacePartition -n $numPolygons -p 1 >> metrics.csv
     echo -e "\tExecuting Naive Polygon algorithm..."
-    ./bin/polygonGenerator -a naivePoly -n $numPolygons -p 1 >> metrics.csv
+    ./build/bin/polygonGenerator -a naivePoly -n $numPolygons -p 1 >> metrics.csv
     echo "Finished generating $numPolygons polygons using every algorithm. [$(($i * 100 / $iterations))% complete]"
     echo "----------------------------------------------------------------------------------------------"
 done
